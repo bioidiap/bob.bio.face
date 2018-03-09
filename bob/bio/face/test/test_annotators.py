6a1c470ddfee0e2e0f2b52b85575eada40ac32a2
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-03-09 16:08:09 +0100

    improve tests

--- bob/bio/face/test/test_annotators.py
+++ bob/bio/face/test/test_annotators.py
@@ -3,6 +3,7 @@ import bob.io.base
 import bob.io.base.test_utils
 import bob.io.image
 import bob.ip.facedetect
+import numpy as np
 
 face_image = bob.io.base.load(bob.io.base.test_utils.datafile(
     'testimage.jpg', 'bob.ip.facedetect'))
@@ -10,4 +11,7 @@ face_image = bob.io.base.load(bob.io.base.test_utils.datafile(
 
 def test_bob_ip_facedetect():
     from bob.bio.face.annotator.bobipfacedetect import BobIpFacedetect
-    annot = BobIpFacedetect(face_image)
+    annot = BobIpFacedetect()(face_image)
+    assert annot['topleft'] == (110, 82), annot
+    assert annot['bottomright'] == (334, 268), annot
+    assert np.allclose(annot['quality'], 39.209601948013685), annot

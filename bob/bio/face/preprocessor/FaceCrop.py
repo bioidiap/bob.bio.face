bef0c3e1ed62888b1f1c12d477adf92243f8cbdf
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-03-09 15:24:41 +0100

    Handle cases when annotations is None in FaceCrop Fixes #28

--- bob/bio/face/preprocessor/FaceCrop.py
+++ bob/bio/face/preprocessor/FaceCrop.py
@@ -19,9 +19,13 @@
 
 import bob.ip.base
 import numpy
+import logging
 
 from .Base import Base
 from bob.bio.base.preprocessor import Preprocessor
+
+logger = logging.getLogger(__name__)
+
 
 class FaceCrop (Base):
   """Crops the face according to the given annotations.
@@ -144,8 +148,6 @@ class FaceCrop (Base):
     face : 2D :py:class:`numpy.ndarray` (float)
       The cropped face.
     """
-    if self.fixed_positions is not None:
-      annotations = self.fixed_positions
     if annotations is None:
       raise ValueError("Cannot perform image cropping since annotations are not given, and no fixed annotations are specified.")
 
@@ -202,6 +204,12 @@ class FaceCrop (Base):
     face : 2D :py:class:`numpy.ndarray`
       The cropped face.
     """
+    if self.fixed_positions is not None:
+      annotations = self.fixed_positions
+
+    if not annotations:
+      logger.warn("Cannot crop face without annotations. Returning None.")
+      return None
     # convert to the desired color channel
     image = self.color_channel(image)
     # crop face

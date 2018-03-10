49a61fa60d313bc5ca90e7a73da5398d90375387
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-03-10 11:09:55 +0100

    Remove extra spacing change

--- setup.py
+++ setup.py
@@ -180,7 +180,6 @@ setup(
             'histogram         = bob.bio.face.config.algorithm.histogram:algorithm',  # LGBPHS histograms
             'bic-jets          = bob.bio.face.config.algorithm.bic_jets:algorithm',  # BIC on gabor jets
         ],
-
     },
 
     # Classifiers are important if you plan to distribute this package through

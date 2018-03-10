9a6771beb44662c6eb7b257dc0f92eeb001809db
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-03-10 11:07:47 +0100

    Use numpy instead of np

--- bob/bio/face/test/test_annotators.py
+++ bob/bio/face/test/test_annotators.py
@@ -3,7 +3,7 @@ import bob.io.base
 import bob.io.base.test_utils
 import bob.io.image
 import bob.ip.facedetect
-import numpy as np
+import numpy
 
 face_image = bob.io.base.load(bob.io.base.test_utils.datafile(
     'testimage.jpg', 'bob.ip.facedetect'))
@@ -14,4 +14,4 @@ def test_bob_ip_facedetect():
     annot = BobIpFacedetect()(face_image)
     assert annot['topleft'] == (110, 82), annot
     assert annot['bottomright'] == (334, 268), annot
-    assert np.allclose(annot['quality'], 39.209601948013685), annot
+    assert numpy.allclose(annot['quality'], 39.209601948013685), annot

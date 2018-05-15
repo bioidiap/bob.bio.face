1d866652e17dff5abed59db09031bba97e2a119a
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2018-05-15 14:19:23 +0200

    Removing baslines
    Removed baselines

--- bob/bio/face/test/test_scripts.py
+++ bob/bio/face/test/test_scripts.py
@@ -1,28 +1,6 @@
 import bob.bio.base.test.utils
 import bob.bio.face
 
-@bob.bio.base.test.utils.grid_available
-def test_baselines():
-  # test that all of the baselines would execute
-  from bob.bio.face.script.baselines import available_databases, all_algorithms, main
-
-  with bob.bio.base.test.utils.Quiet():
-    for database in available_databases:
-      parameters = ['-d', database, '--dry-run']
-      main(parameters)
-      parameters.append('--grid')
-      main(parameters)
-      parameters.extend(['-e', 'HTER'])
-      main(parameters)
-
-    for algorithm in all_algorithms:
-      parameters = ['-a', algorithm, '--dry-run']
-      main(parameters)
-      parameters.append('-g')
-      main(parameters)
-      parameters.extend(['-e', 'HTER'])
-      main(parameters)
-
 def test_display_annotations():
 
   from bob.bio.face.script.display_face_annotations import main

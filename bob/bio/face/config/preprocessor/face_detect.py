cc5c340ed3a83227fea8e16cdb0b4c5aa30fcd6f
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-02-24 16:16:01 +0100

    [dask] Making some entry-points picklable

--- bob/bio/face/config/preprocessor/face_detect.py
+++ bob/bio/face/config/preprocessor/face_detect.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
 import bob.bio.face
+import functools
 
 # Detects the face and eye landmarks crops it using the detected eyes
-preprocessor = bob.bio.face.preprocessor.FaceDetect(
+preprocessor = functools.partial(bob.bio.face.preprocessor.FaceDetect,
   face_cropper = 'face-crop-eyes',
   use_flandmark = True
 )
 
 # Detects the face amd crops it without eye detection
-preprocessor_no_eyes = bob.bio.face.preprocessor.FaceDetect(
+preprocessor_no_eyes = functools.partial(bob.bio.face.preprocessor.FaceDetect,
   face_cropper = 'face-crop-eyes',
   use_flandmark = False
 )

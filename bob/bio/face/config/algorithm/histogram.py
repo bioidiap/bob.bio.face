c2076acec64307f133d30ffce3180ffab41c160f
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-06-16 14:54:59 +0200

    Cleaning up some unecessary entrypoints

--- bob/bio/face/config/algorithm/histogram.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import bob.bio.face
-import bob.math
-
-algorithm = bob.bio.face.algorithm.Histogram(
-    distance_function = bob.math.histogram_intersection,
-    is_distance_function = False
-)
--- bob/bio/face/config/extractor/dct_blocks.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import bob.bio.face
-
-extractor = bob.bio.face.extractor.DCTBlocks(
-    block_size = 12,
-    block_overlap = 11,
-    number_of_dct_coefficients = 45
-)

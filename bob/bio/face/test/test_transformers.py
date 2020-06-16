09ba5486efdfb1ed54d287d13f847e15af432271
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-06-16 17:24:51 +0200

    TEsted LGBPHS transformer

--- bob/bio/face/test/test_transformers.py
+++ bob/bio/face/test/test_transformers.py
@@ -5,7 +5,7 @@ from bob.pipelines import Sample, SampleSet
 from bob.bio.base import load_resource
 
 def get_fake_sample(face_size=(160, 160), eyes={"leye": (46, 107), "reye": (46, 53)}):
-
+    np.random.seed(10)
     data = np.random.rand(3, 400, 400)
     annotations = {"leye": (115, 267), "reye": (115, 132)}
     return Sample(data, key="1", annotations=annotations)
@@ -82,3 +82,14 @@ def test_gabor_graph():
     transformed_data = transformed_sample.data
 
     assert len(transformed_sample.data) == 80
+
+
+def test_lgbphs():    
+    transformer = load_resource("lgbphs", "transformer")
+
+    fake_sample = get_fake_sample()
+
+    transformed_sample = transformer.transform([fake_sample])[0]
+    transformed_data = transformed_sample.data
+
+    assert transformed_sample.data.shape == (2, 44014)

c147c83491faae3915fd552e3ccc4f20dee46930
Yannick DAYER <yannick.dayer@idiap.ch>
2020-08-11 17:43:24 +0200

    [py][fix] Add annotation_type=None to atnt

--- bob/bio/face/config/database/atnt.py
+++ bob/bio/face/config/database/atnt.py
@@ -7,5 +7,6 @@ from bob.bio.face.database import AtntBioDatabase
 atnt_directory = rc["bob.db.atnt.directory"]
 
 database = DatabaseConnector(
-    AtntBioDatabase(original_directory=atnt_directory, original_extension=".pgm",)
+    AtntBioDatabase(original_directory=atnt_directory, original_extension=".pgm",),
+    annotation_type=None,
 )

3288ba13911e3c818bf4c211aa8768c548d5437d
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-08-28 10:52:03 +0200

    Removed the -T hack from gabor_graph

--- bob/bio/face/config/baseline/lda.py
+++ bob/bio/face/config/baseline/lda.py
@@ -2,7 +2,6 @@ from bob.bio.base.pipelines.vanilla_biometrics import (
     Distance,
     VanillaBiometricsPipeline,
     BioAlgorithmLegacy,
-    temp_directory
 )
 from bob.bio.face.config.baseline.helpers import crop_80x64
 import numpy as np
@@ -28,7 +27,7 @@ else:
 
 
 ####### SOLVING THE FACE CROPPER TO BE USED ##########
-def load(annotation_type, fixed_positions=None, checkpoints_dir=None):
+def load(annotation_type, fixed_positions=None):
 
     # Cropping
     face_cropper, transform_extra_arguments = crop_80x64(
@@ -49,18 +48,6 @@ def load(annotation_type, fixed_positions=None, checkpoints_dir=None):
     else:
         # if /idiap/temp/<USER> does not exist, use /tmp/tmpxxxxxxxx
         tempdir = tempfile.TemporaryDirectory().name
-
-    # Replace the default if provided
-    if checkpoints_dir is not None:
-        try:
-            os.makedirs(checkpoints_dir, exist_ok=True)
-        except OSError:
-            logger.info(
-                "Could not create directory '{}'.".format(checkpoints_dir)
-                + " Using default ('{}').".format(tempdir)
-            )
-        else:
-            tempdir = checkpoints_dir
 
     lda = bob.bio.base.algorithm.LDA(use_pinv=True, pca_subspace_dimension=0.90)
 
@@ -88,10 +75,6 @@ def load(annotation_type, fixed_positions=None, checkpoints_dir=None):
 
     return VanillaBiometricsPipeline(transformer, algorithm)
 
-try: temp_directory
-except NameError:
-    logger.info("Temporary directory not defined. Using default.")
-    pipeline = load(annotation_type, fixed_positions, None)
-else:
-    pipeline = load(annotation_type, fixed_positions, temp_directory)
+pipeline = load(annotation_type, fixed_positions)
+
 transformer = pipeline.transformer

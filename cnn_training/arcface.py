dad653499fed659542aaee02a661e6da3f0e7e32
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2021-01-29 18:00:53 +0100

    Update TFTraining

--- cnn_training/arcface.py
+++ cnn_training/arcface.py
@@ -18,27 +18,11 @@ SCALE = 64.0  # Scale
 
 
 Usage:
-    arcface.py <tf_record_paths> <checkpoint_path> [--dropout-rate=<kn> --bottleneck=<kn> --lr=<kn> --solver=<kn> --backbone=<kn> --n-classes=<kn> --epochs=<kn> --batch-size=<kn>] arcface [--m=<kn> --s=<kn>]
-    arcface.py <tf_record_paths> <checkpoint_path> [--dropout-rate=<kn> --bottleneck=<kn> --lr=<kn> --solver=<kn> --backbone=<kn> --n-classes=<kn> --epochs=<kn> --batch-size=<kn>] arcface-3p [--m1=<kn> --m2=<kn> --m3=<kn> --s=<kn>]
-    arcface.py <tf_record_paths> <checkpoint_path> [--dropout-rate=<kn> --bottleneck=<kn> --lr=<kn> --solver=<kn> --backbone=<kn> --n-classes=<kn> --epochs=<kn> --batch-size=<kn>] sphereface [--m=<kn>]
-    arcface.py <tf_record_paths> <checkpoint_path> [--dropout-rate=<kn> --bottleneck=<kn> --lr=<kn> --solver=<kn> --backbone=<kn> --n-classes=<kn> --epochs=<kn> --batch-size=<kn>] modified-softmax
+    arcface.py <config-yaml> <checkpoint_path> 
     arcface.py -h | --help
 
 Options:
   -h --help             Show this screen.
-  --m=<kn>              ArcFace/SphereFace Margin [default: 0.5]
-  --m1=<kn>             ArcFace Margin [default: 1.0]
-  --m2=<kn>             ArcFace Margin [default: 0.3]
-  --m3=<kn>             ArcFace Margin [default: 0.2]
-  --s=<kn>              ArcFace Scale [default: 64] 
-  --n-classes=<kn>      Number of classes [default: 87662]
-  --epochs=<kn>         Epochs [default: 35]
-  --lr=<kn>             Learning Rate [default: 0.1]
-  --batch-size=<kn>     Batch size [default: 90]
-  --backbone=<kn>       DCNN Backbone (Options available are: `inception-resnet-v2`, `efficientnet-B0`, `resnet50` and `mobilenet-v2`) [default: inception-resnet-v2]
-  --solver=<kn>         Solver used during training (Options available are: `rmsprop`, `adam`, `adagrad`) [default: adam]
-  --bottleneck=<kn>     Bottleneck size [default: 128]
-  --dropout-rate=<kn>   Dropout Rate used before the bottleneck [default: 0.2]
   arcface.py arcface -h | help
 
 """
@@ -55,6 +39,7 @@ from bob.learn.tensorflow.callbacks import add_backup_callback
 from bob.learn.tensorflow.metrics.embedding_accuracy import accuracy_from_embeddings
 from bob.extension import rc
 from bob.bio.face.tensorflow.preprocessing import prepare_dataset
+import yaml
 
 from bob.learn.tensorflow.layers import (
     add_bottleneck,
@@ -71,13 +56,19 @@ from bob.learn.tensorflow.models import (
 )
 
 
-# CNN Backbone
-# Change your NN backbone here
+##############################
+# CNN Backbones
+# Add your NN backbone here
+##############################
 BACKBONES = dict()
 BACKBONES["inception-resnet-v2"] = InceptionResNetV2
 BACKBONES["efficientnet-B0"] = tf.keras.applications.EfficientNetB0
 BACKBONES["resnet50"] = tf.keras.applications.ResNet50
 BACKBONES["mobilenet-v2"] = tf.keras.applications.MobileNetV2
+
+##############################
+# SOLVER SPECIFICATIONS
+##############################
 
 SOLVERS = dict()
 # Parameters taken from https://github.com/davidsandberg/facenet/blob/master/src/facenet.py#L181
@@ -92,33 +83,48 @@ SOLVERS["rmsprop"] = partial(
 )
 SOLVERS["adam"] = partial(tf.keras.optimizers.Adam, learning_rate=learning_rate)
 SOLVERS["adagrad"] = partial(tf.keras.optimizers.Adagrad, learning_rate=learning_rate)
+SOLVERS["sgd"] = partial(
+    tf.keras.optimizers.SGD, learning_rate=learning_rate, momentum=0.9, nesterov=True
+)
+
+
+################################
+# DATA SPECIFICATION
+###############################
+DATA_SHAPES = dict()
+
+# Inputs with 182x182 are cropped to 160x160
+DATA_SHAPES[182] = 160
+DATA_SHAPES[112] = 112
 
 
 # SHAPES EXPECTED FROM THE DATASET USING THIS BACKBONE
-DATA_SHAPE = (182, 182, 3)  # size of faces
+# DATA_SHAPE = (182, 182, 3)  # size of faces
 DATA_TYPE = tf.uint8
-OUTPUT_SHAPE = (160, 160)
+# OUTPUT_SHAPE = (160, 160)
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 # HERE WE VALIDATE WITH LFW RUNNING A
 # INFORMATION ABOUT THE VALIDATION SET
-VALIDATION_TF_RECORD_PATHS = rc["bob.bio.face.cnn.lfw_tfrecord_path"]
+# VALIDATION_TF_RECORD_PATHS = rc["bob.bio.face.cnn.lfw_tfrecord_path"]
 
 # there are 2812 samples in the validation set
 VALIDATION_SAMPLES = 2812
 VALIDATION_BATCH_SIZE = 38
 
 
-def create_model(n_classes, model_spec, backbone, bottleneck, dropout_rate):
+def create_model(
+    n_classes, model_spec, backbone, bottleneck, dropout_rate, input_shape
+):
 
     if backbone == "inception-resnet-v2":
         pre_model = BACKBONES[backbone](
-            include_top=False, bottleneck=False, input_shape=OUTPUT_SHAPE + (3,),
+            include_top=False, bottleneck=False, input_shape=input_shape,
         )
     else:
         pre_model = BACKBONES[backbone](
-            include_top=False, input_shape=OUTPUT_SHAPE + (3,), weights=None,
+            include_top=False, input_shape=input_shape, weights=None,
         )
 
     # Adding the bottleneck
@@ -129,7 +135,9 @@ def create_model(n_classes, model_spec, backbone, bottleneck, dropout_rate):
 
     float32_layer = layers.Activation("linear", dtype="float32")
 
-    embeddings = tf.nn.l2_normalize(pre_model.get_layer("embeddings").output, axis=1)
+    embeddings = tf.nn.l2_normalize(
+        pre_model.get_layer("embeddings/BatchNorm").output, axis=1
+    )
 
     logits_premodel = float32_layer(pre_model.get_layer("logits").output)
 
@@ -178,10 +186,10 @@ def create_model(n_classes, model_spec, backbone, bottleneck, dropout_rate):
 
 
 def build_and_compile_models(
-    n_classes, optimizer, model_spec, backbone, bottleneck, dropout_rate
+    n_classes, optimizer, model_spec, backbone, bottleneck, dropout_rate, input_shape
 ):
     pre_model, arc_model = create_model(
-        n_classes, model_spec, backbone, bottleneck, dropout_rate
+        n_classes, model_spec, backbone, bottleneck, dropout_rate, input_shape
     )
 
     cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(
@@ -206,12 +214,26 @@ def train_and_evaluate(
     optimizer,
     bottleneck,
     dropout_rate,
+    face_size,
+    validation_path,
 ):
+
     # number of training steps to do before validating a model. This also defines an epoch
     # for keras which is not really true. We want to evaluate every 180000 (90 * 2000)
     # samples
-    STEPS_PER_EPOCH = 180000 // batch_size
-    KERAS_EPOCH_MULTIPLIER = 6
+    # STEPS_PER_EPOCH = 180000 // batch_size
+    # KERAS_EPOCH_MULTIPLIER = 6
+    STEPS_PER_EPOCH = 2000
+
+    DATA_SHAPE = (face_size, face_size, 3)
+    OUTPUT_SHAPE = (DATA_SHAPES[face_size], DATA_SHAPES[face_size])
+
+    if validation_path is None:
+        validation_path = rc["bob.bio.face.cnn.lfw_tfrecord_path"]
+        if validation_path is None:
+            raise ValueError(
+                "No validation set was set. Please, do `bob config set bob.bio.face.cnn.lfw_tfrecord_path [PATH]`"
+            )
 
     train_ds = prepare_dataset(
         tf_record_paths,
@@ -223,13 +245,8 @@ def train_and_evaluate(
         augment=True,
     )
 
-    if VALIDATION_TF_RECORD_PATHS is None:
-        raise ValueError(
-            "No validation set was set. Please, do `bob config set bob.bio.face.cnn.lfw_tfrecord_path [PATH]`"
-        )
-
     val_ds = prepare_dataset(
-        VALIDATION_TF_RECORD_PATHS,
+        validation_path,
         data_shape=DATA_SHAPE,
         output_shape=OUTPUT_SHAPE,
         epochs=epochs,
@@ -246,22 +263,21 @@ def train_and_evaluate(
         backbone,
         bottleneck=bottleneck,
         dropout_rate=dropout_rate,
+        input_shape=OUTPUT_SHAPE + (3,),
     )
 
     def scheduler(epoch, lr):
-        # 20 epochs at 0.1, 10 at 0.01 and 5 0.001
+        # 200 epochs at 0.1, 10 at 0.01 and 5 0.001
         # The epoch number here is Keras's which is different from actual epoch number
-        epoch = epoch // KERAS_EPOCH_MULTIPLIER
+        # epoch = epoch // KERAS_EPOCH_MULTIPLIER
 
         # Tracking in the tensorboard
         tf.summary.scalar("learning rate", data=lr, step=epoch)
 
-        if epoch in range(20):
-            return 0.1
-        elif epoch in range(20, 30):
-            return 0.01
+        if epoch in range(200):
+            return 1 * lr
         else:
-            return 0.001
+            return lr * tf.math.exp(-0.1)
 
     callbacks = {
         "latest": tf.keras.callbacks.ModelCheckpoint(
@@ -278,7 +294,7 @@ def train_and_evaluate(
     # STEPS_PER_EPOCH
     pre_model.fit(
         train_ds,
-        epochs=1,
+        epochs=2,
         validation_data=val_ds,
         steps_per_epoch=STEPS_PER_EPOCH,
         validation_steps=VALIDATION_SAMPLES // VALIDATION_BATCH_SIZE,
@@ -291,7 +307,7 @@ def train_and_evaluate(
     arc_model.fit(
         train_ds,
         validation_data=val_ds,
-        epochs=epochs * KERAS_EPOCH_MULTIPLIER,
+        epochs=epochs,
         steps_per_epoch=STEPS_PER_EPOCH,
         validation_steps=VALIDATION_SAMPLES // VALIDATION_BATCH_SIZE,
         callbacks=callbacks,
@@ -304,37 +320,41 @@ from docopt import docopt
 if __name__ == "__main__":
     args = docopt(__doc__)
 
+    config = yaml.full_load(open(args["<config-yaml>"]))
+
     model_spec = dict()
-    if args["arcface"]:
+    if config["head"] == "arcface":
         model_spec["arcface"] = dict()
-        model_spec["arcface"]["m"] = float(args["--m"])
-        model_spec["arcface"]["s"] = int(args["--s"])
+        model_spec["arcface"]["m"] = float(config["m"])
+        model_spec["arcface"]["s"] = int(config["s"])
 
-    if args["arcface-3p"]:
+    if config["head"] == "arcface-3p":
         model_spec["arcface-3p"] = dict()
-        model_spec["arcface-3p"]["m1"] = float(args["--m1"])
-        model_spec["arcface-3p"]["m2"] = float(args["--m2"])
-        model_spec["arcface-3p"]["m3"] = float(args["--m3"])
-        model_spec["arcface-3p"]["s"] = int(args["--s"])
+        model_spec["arcface-3p"]["m1"] = float(config["m1"])
+        model_spec["arcface-3p"]["m2"] = float(config["m2"])
+        model_spec["arcface-3p"]["m3"] = float(config["m3"])
+        model_spec["arcface-3p"]["s"] = int(config["s"])
 
-    if args["sphereface"]:
+    if config["head"] == "sphereface":
         model_spec["sphereface"] = dict()
-        model_spec["sphereface"]["m"] = float(args["--m"])
+        model_spec["sphereface"]["m"] = float(config["m"])
 
-    if args["modified-softmax"]:
+    if config["head"] == "modified-softmax":
         # There's no hyper parameter here
         model_spec["modified-softmax"] = dict()
 
     train_and_evaluate(
-        args["<tf_record_paths>"],
+        config["train-tf-record-path"],
         args["<checkpoint_path>"],
-        int(args["--n-classes"]),
-        int(args["--batch-size"]),
-        int(args["--epochs"]),
+        int(config["n-classes"]),
+        int(config["batch-size"]),
+        int(config["epochs"]),
         model_spec,
-        args["--backbone"],
-        optimizer=SOLVERS[args["--solver"]](learning_rate=float(args["--lr"])),
-        bottleneck=int(args["--bottleneck"]),
-        dropout_rate=float(args["--dropout-rate"]),
+        config["backbone"],
+        optimizer=SOLVERS[config["solver"]](learning_rate=float(config["lr"])),
+        bottleneck=int(config["bottleneck"]),
+        dropout_rate=float(config["dropout-rate"]),
+        face_size=int(config["face-size"]),
+        validation_path=config["validation-tf-record-path"],
     )
 

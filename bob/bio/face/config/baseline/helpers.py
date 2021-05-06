c2a907ccfb2eb8fbd4349956d707e06d4dedc66b
Laurent COLBOIS <lcolbois@.idiap.ch>
2021-05-06 09:10:11 +0200

    Regrouping config  helper modules

--- bob/bio/face/config/baseline/helpers.py
+++ bob/bio/face/config/baseline/helpers.py
@@ -1,12 +1,48 @@
 import bob.bio.face
+from bob.bio.face.preprocessor import FaceCrop, MultiFaceCrop, Scale
 from sklearn.pipeline import make_pipeline
 from bob.bio.base.wrappers import wrap_sample_preprocessor
 from bob.pipelines import wrap
-from bob.bio.face.helpers import face_crop_solver
 import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
+
+
+def face_crop_solver(
+    cropped_image_size,
+    cropped_positions=None,
+    color_channel="rgb",
+    fixed_positions=None,
+    annotator=None,
+    dtype="uint8",
+):
+    """
+    Decide which face cropper to use.
+    """
+    # If there's not cropped positions, just resize
+    if cropped_positions is None:
+        return Scale(cropped_image_size)
+    else:
+        # Detects the face and crops it without eye detection
+        if isinstance(cropped_positions, list):
+            return MultiFaceCrop(
+                cropped_image_size=cropped_image_size,
+                cropped_positions_list=cropped_positions,
+                fixed_positions_list=fixed_positions,
+                color_channel=color_channel,
+                dtype=dtype,
+                annotation=annotator,
+            )
+        else:
+            return FaceCrop(
+                cropped_image_size=cropped_image_size,
+                cropped_positions=cropped_positions,
+                color_channel=color_channel,
+                fixed_positions=fixed_positions,
+                dtype=dtype,
+                annotator=annotator,
+            )
 
 
 def embedding_transformer_default_cropping(cropped_image_size, annotation_type):

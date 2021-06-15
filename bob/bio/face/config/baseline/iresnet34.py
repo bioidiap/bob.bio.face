4e2e9722f91793f7d1d25c862a46c45e9448cce9
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2021-06-15 18:59:27 +0200

    Add memory demanding to pytorch models

--- bob/bio/face/config/baseline/iresnet34.py
+++ bob/bio/face/config/baseline/iresnet34.py
@@ -2,14 +2,14 @@ from bob.bio.face.embeddings.pytorch import iresnet34
 from bob.bio.face.utils import lookup_config_from_database
 
 
-annotation_type, fixed_positions, _ = lookup_config_from_database(
+annotation_type, fixed_positions, memory_demanding = lookup_config_from_database(
     locals().get("database")
 )
 
 
-def load(annotation_type, fixed_positions=None):
-    return iresnet34(annotation_type, fixed_positions)
+def load(annotation_type, fixed_positions=None, memory_demanding=False):
+    return iresnet34(annotation_type, fixed_positions, memory_demanding)
 
 
-pipeline = load(annotation_type, fixed_positions)
+pipeline = load(annotation_type, fixed_positions, memory_demanding)
 
--- bob/bio/face/config/baseline/iresnet50.py
+++ bob/bio/face/config/baseline/iresnet50.py
@@ -2,14 +2,14 @@ from bob.bio.face.embeddings.pytorch import iresnet50
 from bob.bio.face.utils import lookup_config_from_database
 
 
-annotation_type, fixed_positions, _ = lookup_config_from_database(
+annotation_type, fixed_positions, memory_demanding = lookup_config_from_database(
     locals().get("database")
 )
 
 
-def load(annotation_type, fixed_positions=None):
-    return iresnet50(annotation_type, fixed_positions)
+def load(annotation_type, fixed_positions=None, memory_demanding=False):
+    return iresnet50(annotation_type, fixed_positions, memory_demanding)
 
 
-pipeline = load(annotation_type, fixed_positions)
+pipeline = load(annotation_type, fixed_positions, memory_demanding)
 

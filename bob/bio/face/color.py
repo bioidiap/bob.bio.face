f99b75219925386d26c3c880dce0bdec553e4ea7
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2022-04-26 14:08:45 +0200

    [color] add rgb_to_yuv and rgb_to_hsv

--- bob/bio/face/color.py
+++ bob/bio/face/color.py
@@ -50,3 +50,76 @@ def gray_to_rgb(image):
     assert image.ndim == 2, "The image should have 2 dimensions"
 
     return np.stack((image, image, image), axis=0)
+
+
+def rgb_to_hsv(image):
+    """
+    Converts an RGB image to an HSV image.
+
+    Parameters
+    ----------
+
+    image : numpy.ndarray
+        An image in RGB format (channels first): For an ND array (N >= 3),
+
+
+    """
+
+    assert image.ndim == 3, "The image should have 3 dimensions"
+
+    image_hsv = np.zeros(image.shape)
+
+    R = image[0, :, :]
+    G = image[1, :, :]
+    B = image[2, :, :]
+
+    max_value = np.max(image, axis=0)
+    min_value = np.min(image, axis=0)
+    delta = max_value - min_value
+
+    # Hue
+    image_hsv[0, :, :] = np.arctan2(B - G, R - G)
+    image_hsv[0, :, :] = (image_hsv[0, :, :] + np.pi) % (2 * np.pi)
+
+    # Saturation
+    image_hsv[1, :, :] = np.maximum(max_value, 0.00001)
+    image_hsv[1, :, :] /= np.maximum(delta, 0.00001)
+    image_hsv[1, :, :] = np.minimum(image_hsv[1, :, :], 1.0)
+
+    # Value
+    image_hsv[2, :, :] = max_value
+
+    return image_hsv
+
+
+def rgb_to_yuv(image):
+    """
+    Converts an RGB image to an YUV image.
+
+    Parameters
+    ----------
+
+    image : numpy.ndarray
+        An image in RGB format (channels first): For an ND array (N >= 3),
+
+
+    """
+
+    assert image.ndim == 3, "The image should have 3 dimensions"
+
+    image_yuv = np.zeros(image.shape)
+
+    R = image[0, :, :]
+    G = image[1, :, :]
+    B = image[2, :, :]
+
+    # Y
+    image_yuv[0, :, :] = 0.299 * R + 0.587 * G + 0.114 * B
+
+    # U
+    image_yuv[1, :, :] = 0.492 * (B - image_yuv[0, :, :])
+
+    # V
+    image_yuv[2, :, :] = 0.877 * (R - image_yuv[0, :, :])
+
+    return image_yuv
